1d55ef9ad644dacfb1b9e86794d1278b32fdb01d
Shibsankar Pramanik <shibsankar.pramanik@oracle.com>
2021-08-31 23:11:43 +0530

    modified

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -5,22 +5,14 @@ class EmotionTable extends React.Component {
     render() {
       //Returns the emotions as an HTML table
       return (  
-        <div>
-          <table className="table table-bordered">
-            <tbody>
-            {
-              resObject.map((value) =>
-                                <>
-                                    <tr>
-                                        <td>{value[0]}</td>
-                                        <td>{value[1]}</td>
-                                    </tr>
-                                </>
-                            )
-            }
-            </tbody>
-          </table>
-          </div>
+        Object.entries(this.props.emotions).map(function(mapentry) {
+    return (
+        <tr>
+        <td>{mapentry[0]}</td>
+        <td>{mapentry[1]}</td>
+        </tr>
+    )
+    })
           );
         }
     
